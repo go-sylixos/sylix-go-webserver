f5a5023fd0e5412ee74f092519238ca46dc8e5f9
chengsiyuan <chengsiyuan@acoinfo.com>
2025-01-09 09:35:24 +0000

    add func run(router *gin.Engine) doc.

--- main.go
+++ main.go
@@ -22,6 +22,15 @@ func main() {
 //go:embed static/*
 var staticFS embed.FS
 
+// 初始化给定的 gin.Engine 路由器，设置各种路由和静态文件处理。
+//
+// 它设置了以下路由：
+// - 从 "static" 目录在 "/static" URL 路径提供静态文件。
+// - 一个根路由，从 "static" 目录提供 "index.html" 文件。
+// - 一个处理 404 错误的回退路由，通过提供静态文件。
+// - 一个 ACL（访问控制列表）路由组。
+// - 一个带有 "/index" 端点的 API 路由组，返回欢迎消息。
+// - 一个位于 "/api/v1" 下的业务 API 路由组。
 func run(router *gin.Engine) { // http://localhost:10000/static/index.html
 	static, _ := fs.Sub(staticFS, "static")
 	router.StaticFS("/static", http.FS(static))
